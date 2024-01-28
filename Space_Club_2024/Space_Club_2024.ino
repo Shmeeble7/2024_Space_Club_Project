9462e820317d71602dee41ab1375ad0e4b42b4e9
Ian Bailey <ianb33997@gmail.com>
2024-01-28 12:25:23 -0500

    Day 1 temp changes

--- Space_Club_2024/Space_Club_2024.ino
+++ Space_Club_2024/Space_Club_2024.ino
@@ -1,9 +1,47 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * 
+ * Insert basic team and project info
+ * dont forget this time
+ * 
+ */
+ 
+#include <stdint.h>
+#include <Wire.h>
+#include <WireIMXRT.h>
+#include <WireKinetis.h>
+#include "LIDARLite_v4LED.h"
+#include <Adafruit_Sensor.h>
+#include <SD.h>
+#include <L3G.h>
+//Gyroscope, 3 axis, records rotation data
+#include <Adafruit_L3GD20.h>
+#include <Adafruit_L3GD20_U.h>
+//Magnetometer, records and measures magnetic fields
+#include <Adafruit_MMC56x3.h>
+//Accelerometer, 3 axis, records acceleration on all three axis.
+#include <Adafruit_ADXL375.h>
+
+//Our gyroscope object
+Adafruit_L3GD20 gyro;
+
+Adafruit_MMC56x3 magnet;
+
+Adafruit_ADXL375 accelerometer;
+
+int sd = BUILTIN_SDCARD;
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void setup() 
+{
+  // put yuor setup code here, to run once:
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
 }
